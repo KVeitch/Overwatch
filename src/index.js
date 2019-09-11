6870304843521ee2f5b020dd0c31e55195009799
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-09-11 16:53:27 -0600

    Set up initial files and testing suite

--- src/index.js
+++ src/index.js
@@ -3,11 +3,31 @@
 
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
-
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
-
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
+let roomServices, userData, roomData, bookings;
+// roomservice data
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices')
+  .then(data => data.json())
+  .then(data => {roomServices = data.roomServices})
+  .then(()=>{console.log('roomServices',roomServices)})
+
+// .then(data => {assignRoomServiceData(data.roomServices)})
+
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
+  .then(data => data.json())
+  .then(data => {userData = data.users})
+  // .then(()=>{console.log('userData',userData)})
+
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
+  .then(data => data.json())
+  .then(data => {roomData = data.rooms})
+  // .then(()=>{console.log('roomData',roomData)})
+
+fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
+  .then(data => data.json())
+  .then(data => {bookings = data.bookings})
+  // .then(()=>{console.log('bookings',bookings)})
---
+++ test/index-testing.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+const expect = chai.expect;
+import Booking from '../src/booking';
+
+describe('Index', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+
+});
