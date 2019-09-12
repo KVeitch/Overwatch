66413ba9e4df23f2db3aa23d2010f2456353b467
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-09-12 12:18:07 -0600

    Add initial html and tab switching and colot pallet for css

--- src/index.js
+++ src/index.js
@@ -19,15 +19,31 @@ fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServ
 
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users')
   .then(data => data.json())
-  .then(data => {userData = data.users})
+  .then(data => {userData = data.users});
   // .then(()=>{console.log('userData',userData)})
 
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms')
   .then(data => data.json())
-  .then(data => {roomData = data.rooms})
+  .then(data => {roomData = data.rooms});
   // .then(()=>{console.log('roomData',roomData)})
 
 fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings')
   .then(data => data.json())
   .then(data => {bookings = data.bookings})
-  // .then(()=>{console.log('bookings',bookings)})
+  .then(()=>{console.log('bookings',bookings)});
+
+
+
+  // Show the first tab by default
+$('.tabs-stage div').hide();
+$('.tabs-stage div:first').show();
+$('.tabs-nav .button:first').addClass('tab-active');
+
+// Change tab class and display content
+$('.tabs-nav a').on('click', function(event){
+  event.preventDefault();
+  $('.tabs-nav .button').removeClass('tab-active');
+  $(this).parent().addClass('tab-active');
+  $('.tabs-stage div').hide();
+  $($(this).attr('href')).show();
+});
