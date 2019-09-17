36d1ee9e7f8672c70b2f9bfdd2534c9284c63a54
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-09-17 14:38:12 -0600

    Complete testing for Guest class

--- test/guest-test.js
+++ test/guest-test.js
@@ -1,18 +1,26 @@
 import chai from 'chai';
 const expect = chai.expect;
 import Guest from '../src/guest';
+import Hotel from '../src/hotel';
+import bookings from './booking-data';
+import users from './guest-data';
+import roomServices from './roomService-data';
+import rooms from './room-data';
+
+let data = [{roomServices:roomServices},{users:users},{rooms:rooms},{bookings:bookings}]
+
 
 describe('Guest', function() {
-
+  let hotel, guest;
+  beforeEach( () => {
+    hotel = new Hotel(data);
+    hotel.getCurrentGuest(1);
+    guest = hotel.currentGuest;
+  });
+  
   it('should be a function', () => {
     expect(Guest).to.be.a('function');
   });
-  let guestData = {
-    id: 1,
-    name: 'Matilde Larson'
-  }
-
-  let guest = new Guest(guestData)
 
   it('should have a name', () => {
     expect(guest.name).to.equal('Matilde Larson');
@@ -20,6 +28,28 @@ describe('Guest', function() {
 
   it('should be have an Id', () => {
     expect(guest.id).to.equal(1);
+  });
+
+  it('should be have an know its bookings', () => {
+    expect(guest.bookings.length).to.equal(19);
+  });
+
+  it('should know its room services orders', () => {
+    expect(guest.roomServices.length).to.equal(1);
+  });
+
+
+  it('should be able to book a room', () => {
+    let room = {
+      userID: 1,
+      date: '2019/09/17',
+      roomNumber: 100
+    };
+    expect(guest.bookings.length).to.equal(19);
+    guest.bookGuestRoom(room);
+    expect(guest.bookings.length).to.equal(20);
+    expect(guest.bookings[19].roomNumber).to.equal(100)
+
   });
 
 });
